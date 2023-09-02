1dba14f59eb316a8d38bc27efd28cc860b0a7ab2
FaustVX <jodu035@gmail.com>
2023-09-02 23:39:04 +0200

    Using Markup syntax for `SymbolsQtyStyles`

--- Setting.cs
+++ Setting.cs
@@ -4,7 +4,7 @@ using System.Text.Json;
 using System.Text.Json.Serialization;
 using Spectre.Console;
 
-public sealed record class Styles(Style? MinQty, Style? NotPresent, Style? QtyFixed, Style? QtyUnknows);
+public sealed record class SymbolsQtyStyles(Style? MinQty, Style? NotPresent, Style? QtyFixed, Style? QtyUnknows);
 
 public sealed partial class Setting
 {
@@ -22,7 +22,6 @@ public sealed partial class Setting
                 new IReadOnlySetConverter<string>(),
                 new IReadOnlySetConverter<int>(),
                 new StyleConverter(),
-                new ColorConverter(),
             },
             TypeInfoResolver = Ext.JSONContext.Default,
         };
@@ -59,7 +58,7 @@ public sealed partial class Setting
     public required bool SortSymbols { get; init; } = true;
 
     [JsonRequired]
-    public required Styles SymbolsQtyStyles { get; init; } = new(new(Color.Yellow), new(Color.Red), new(Color.Green), null);
+    public required SymbolsQtyStyles SymbolsQtyStyles { get; init; } = new(new(Color.Yellow), new(Color.Red), new(Color.Green), null);
 
     private sealed class IReadOnlySetConverter<T> : JsonConverter<IReadOnlySet<T>>
     {
@@ -94,50 +93,13 @@ public sealed partial class Setting
     {
         public override Style? Read(ref Utf8JsonReader reader, Type typeToConvert, JsonSerializerOptions options)
         {
-            if (reader.TokenType != JsonTokenType.StartObject)
-                throw new JsonException();
-
-            reader.Read();
-
-            var style = new Style();
-
-            while (reader.TokenType != JsonTokenType.EndObject)
-            {
-                style = reader.GetString() switch
-                {
-                    "Foreground" => style.Foreground(JsonSerializer.Deserialize<Color>(ref reader, options)!),
-                    "Background" => style.Background(JsonSerializer.Deserialize<Color>(ref reader, options)!),
-                    "Decoration" => style.Decoration(JsonSerializer.Deserialize<Decoration>(ref reader, options)!),
-                    "Link" => style.Link(JsonSerializer.Deserialize<string>(ref reader, options)!),
-                    _ => throw new JsonException(),
-                };
-                reader.Read();
-            }
-
-            return style;
-        }
-
-        public override void Write(Utf8JsonWriter writer, Style value, JsonSerializerOptions options)
-        {
-            var op = new JsonSerializerOptions(options);
-            op.Converters.Remove(this);
-            JsonSerializer.Serialize(writer, value, op);
-        }
-    }
-
-    private sealed class ColorConverter : JsonConverter<Color>
-    {
-        public override Color Read(ref Utf8JsonReader reader, Type typeToConvert, JsonSerializerOptions options)
-        {
             if (reader.TokenType != JsonTokenType.String)
                 throw new JsonException();
 
-            var hex = reader.GetString();
-            var number = Convert.ToInt32(hex, 16);
-            return new((byte)((number >> 16) & 0xFF), (byte)((number >> 8) & 0xFF), (byte)(number & 0xFF));
+            return Style.Parse(reader.GetString()!);
         }
 
-        public override void Write(Utf8JsonWriter writer, Color value, JsonSerializerOptions options)
-        => JsonSerializer.Serialize(writer, value.ToHex(), options);
+        public override void Write(Utf8JsonWriter writer, Style value, JsonSerializerOptions options)
+        => JsonSerializer.Serialize(writer, value.ToMarkup(), options);
     }
 }
